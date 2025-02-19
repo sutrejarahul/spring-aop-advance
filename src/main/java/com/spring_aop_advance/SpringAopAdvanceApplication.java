3821f6579a07dea21ce92cca391ae32ec7d40965
Rahul Sutreja <sutrejarahul0000@gmail.com>
2025-02-19 15:26:40 +0530

    1. initial setup

---
+++ src/main/java/com/spring_aop_advance/SpringAopAdvanceApplication.java
@@ -0,0 +1,13 @@
+package com.spring_aop_advance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAopAdvanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAopAdvanceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring_aop_advance/SpringAopAdvanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring_aop_advance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAopAdvanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
